88b76f478f0d1088ad0d37a71cc2e03a4ae439b1
Ahmad Rofiqi <114473639+ARofiqi@users.noreply.github.com>
2022-12-29 00:43:30 +0700

    Update bangun_datar.py

--- bangun_datar.py
+++ bangun_datar.py
@@ -1,7 +1,7 @@
 def luas_persegi(sisi) :
     return sisi*sisi
 
-def luas_persegi_panjang(panjang,lebar) :
+def luas_persegi_panjang(panjang,lebar):
     return panjang*lebar
 
 def luas_segitiga(alas,tinggi) :
